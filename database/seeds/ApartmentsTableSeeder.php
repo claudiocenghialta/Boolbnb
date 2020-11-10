935062b07cac8e9f3ddfca4e6069a8a44b11f09b
unknown <fioremartina@hotmail.com>
2020-11-10 18:22:07 +0100

    Seeder+Auth+Controller

---
+++ database/seeds/ApartmentsTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Apartment;
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+
+class ApartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+      $usersCount = count(User::all()->toArray()) - 1;
+
+        for ($i=0; $i < 10; $i++) {
+            $newApartment = new Apartment;
+            $newApartment->titolo = $faker->sentence(3);
+            $newApartment->descrizione = $faker->text(500);
+            $newApartment->slug = Str::finish(Str::slug($newApartment->title), rand(1, 1000000));
+            $newApartment->numero_stanze = rand(1,10);
+            $newApartment->numero_letti = rand(1,10);
+            $newApartment->numero_bagni = rand(1,4);
+            $newApartment->mq = rand(40,300);
+            $newApartment->indirizzo = $faker->address();
+            // $newApartment->numero_visite;
+            // $newApartment->attivo = true;
+            // $newApartment->imma = $faker->imageUrl(640,400);
+            $newApartment->user_id = rand(1, $usersCount);
+
+            $newApartment->save();
+        }
+
+    }
+}
