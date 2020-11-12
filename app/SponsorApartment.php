271ef8ab832ace98af6e45411021ce9f732db2ab
MGF2 <fioremartina@hotmail.com>
2020-11-12 10:40:48 +0100

    counter fixed

--- app/SponsorApartment.php
+++ app/SponsorApartment.php
@@ -9,9 +9,9 @@ class SponsorApartment extends Model
   protected $fillable = [
       'sponsor_id', 'apartment_id', 'data_inizio', 'data_fine'
   ];
-
+  //conflitto updated_at/created_at
   public $timestamps = false;
-  
+
   public function apartment()
   {
     return $this->belongsTo('App\Apartment');
